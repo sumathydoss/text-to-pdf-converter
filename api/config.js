700963e7b55ccf1fcccdaa7c7bdf007258cb7fbf
sumathy <sumathy@zooloo.asia>
2025-12-12 15:11:41 +0800

    update

--- api/config.js
+++ api/config.js
@@ -5,7 +5,7 @@
 export default function handler(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Cache-Control', 'public, max-age=3600');
-    
+
     res.status(200).json({
         recaptchaSiteKey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || '6LeuJyksAAAAAG14ND1x7G3gghpAug5G9VRXK2hp'
     });
